3362883cf24a21c740d63844ae9479f5904f58c1
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-06-26 00:57:52 +0200

    end all bonus + fix

--- routes/web.php
+++ routes/web.php
@@ -272,3 +272,35 @@ Route::get('/', function () {
         ]
     ]);
 });
+
+Route::get('/character', function () {
+    return view('character');
+});
+Route::get('/comics', function () {
+    return view('comics');
+});
+Route::get('/movies', function () {
+    return view('movies');
+});
+Route::get('/tv', function () {
+    return view('tv');
+});
+Route::get('/games', function () {
+    return view('games');
+});
+Route::get('/collectibles', function () {
+    return view('collectibles');
+});
+Route::get('/videos', function () {
+    return view('videos');
+});
+Route::get('/fans', function () {
+    return view('fans');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/shop', function () {
+    return view('shop');
+});
+
